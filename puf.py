380252f89e8e2cd34e3fcd53c06ff497f0848ca3
Sara Achour <sachour@DN0a22a60e.SUNet>
2021-10-13 13:01:21 -0700

    Added documentation

--- puf.py
+++ puf.py
@@ -85,18 +85,27 @@ class ODEPuf3:
     def _from_array(self,value_list):
         return dict(zip(self.var_names, value_list))
 
-    def simulate(self, sim_time, samples_per_step=100, record=False):
+    # simulates the system, using the state variables in `self._state` as the initial condition
+    # The simulation runs for `sim_time` units of time and returns a dictionary of computed time
+    # and value samples
+    def simulate(self, sim_time, samples_per_step=100):
         def dt_func(t,vs):
             result = self._derivative(t, self._from_array(vs))
             return self._to_array(result)
 
+        # compute the time between samples and the total number of samples
         dt = 1.0/samples_per_step
         n = math.ceil(sim_time/dt)
 
+        # setup differential equation solver
         solver = ode(dt_func).set_integrator('zvode', method='bdf')
-        x0 = self._to_array(self.state())
 
+        # set initial condition
+        x0 = self._to_array(self.state())
         solver.set_initial_value(x0)
+
+        # record the state at each sample step. The solver may compute the step
+        # at intermediate times to get a good simulation.
         info = {"time":[], "values":[]}
         while solver.successful() and solver.t < sim_time:
             info["time"].append(solver.t)
@@ -107,11 +116,15 @@ class ODEPuf3:
         return info
 
 
+    # saves the variable trajectories from the simulation to a series of files in plots/
+    # The `prefix` argument sets the prefix for each plot. The suffix of the plot
+    # is the state variable the plot shows.
     def save_plots(self,prefix,info):
         directory = "plots"
         if not os.path.exists(directory):
             os.mkdir(directory)
 
+        # translate the data in `info` to a format that can be easily plotted
         times = info["time"]
         variables = {}
         for idx,variable in enumerate(self.var_names):
@@ -119,6 +132,7 @@ class ODEPuf3:
             for vect in info["values"]:
                 variables[variable].append(vect[idx])
 
+        # plot each variable's trajectory and save it to a file
         for name,values in variables.items():
             plt.plot(times,values)
             plt.savefig("%s/%s_%s.png" % (directory,prefix,name))
@@ -126,6 +140,7 @@ class ODEPuf3:
 
 
     def write_challenge(self,pulses):
+        # make sure the inputs make sense
         if not (len(pulses.keys()) == self.number_diffeqs):
             raise Exception("expected <%d> pulses, since there are <%d> diffeqs" \
                             % (self.number_diffeqs))
@@ -140,26 +155,36 @@ class ODEPuf3:
             else:
                 self._forcing_functions[variable] = Zero()
 
+        # set the state variable values to zero
         self.set_to_zero();
+
+        # simulate for 10 simulation time units
         #FIXME.. how do i simulate
         info = self.simulate(10);
+
+        # set the current state of the state variabes in the system
+        # to the last value of each state variable in the simulation
         self.set_state(self._from_array(info["values"][-1]))
         return self.state,info
 
     def get_response(self):
         #FIXME.. how do i get a response
+        # simulate for ten simulation time units and return nothing
         info = self.simulate(10);
         return None,info
 
 
 def test():
     print("making 3-cell puf....")
+    # instantiate the puf with a K parameter for each state variable.
+    # the constructor will check to make sure every variable has a k parameter.
     puf3 = ODEPuf3({"x0":0.1,"x1": 0.2, "x2": 0.3, "v0": 1.0, "v1": 0.24, "v2":0.01})
 
     # instantiate state variables to zero
     puf3.set_to_zero()
 
     # write a challenge to the puf
+    # each puf is labelled with the variable it is provided to
     state,info = puf3.write_challenge({ \
                                         "x0":Pulse(width=1.0,height=1.0), \
                                         "v0":Pulse(width=1.0,height=2.0), \
@@ -168,9 +193,15 @@ def test():
                                         "x2":Pulse(width=3.0,height=1.0), \
                                         "v2":Pulse(width=0.5,height=1.0)})
 
+    # save the variable trajectories for the challenge phase
+    # all plots are written to the "plots/" directory
     puf3.save_plots("challenge",info);
 
+    # collect the response to the challenge
     response,info = puf3.get_response()
+
+    # save the variable trajectories for the response phase
+    # all plots are written to the "plots/" directory
     puf3.save_plots("response",info);
 
 test()
